4191467aea16f5642ed9a53a8e8d4eb66b187cb3
karirogg <karirogg@gmail.com>
2022-02-04 13:45:41 +0000

    Added CSS to enhance look

--- src/make-html.js
+++ src/make-html.js
@@ -4,23 +4,49 @@ export function makeHTML(stats, numberList, fileName) {
   if(Number.isNaN(stats.max)) {
     return `
       <section>
-        <p>Gagnasett inniheldur einungis brengluð gildi eða er innihaldslaust!</p>
+        <p class="no-analysis">Gagnasett inniheldur einungis brengluð gildi eða er innihaldslaust!</p>
       </section>
     `
   }
 
   const template = `
-    <section>
-      <p><strong>Stærsta gildi:</strong> ${stats.max}</p>
-      <p><strong>Minnsta gildi:</strong> ${stats.min}</p>
-      <p><strong>Summa:</strong> ${stats.sum}</p>
-      <p><strong>Meðaltal:</strong> ${stats.mean}</p>
-      <p><strong>Miðgildi:</strong> ${stats.median}</p>
-      <p><strong>Svið:</strong> ${stats.range[0]} - ${stats.range[1]}</p>
-      <p><strong>Frávik:</strong> ${Number.isNaN(stats.variance) ? "Ekki hægt að reikna frávik" : stats.variance}</p>
-      <p><strong>Staðalfrávik:</strong> ${Number.isNaN(stats.std) ? "Ekki hægt að reikna frávik" : stats.std}</p>
+    <section class="numerical-analysis">
+      <h2>Töluleg greining</h2>
+      <div>
+        <p><strong>Stærsta gildi:</strong></p>
+        <p>${stats.max}</p>
+      </div>
+      <div>
+        <p><strong>Minnsta gildi:</strong></p>
+        <p>${stats.min}</p>
+      </div>
+      <div>
+        <p><strong>Summa:</strong></p>
+        <p>${stats.sum.toFixed(3)}</p>
+      </div>
+      <div>
+        <p><strong>Meðaltal:</strong></p>
+        <p>${stats.mean.toFixed(3)}</p>
+      </div>
+      <div>
+        <p><strong>Miðgildi:</strong></p>
+        <p>${stats.median}</p>
+      </div>
+      <div>
+        <p><strong>Svið:</strong></p>
+        <p>${stats.range[0]} - ${stats.range[1]}</p>
+      </div>
+      <div>
+        <p><strong>Frávik:</strong></p>
+        <p>${Number.isNaN(stats.variance) ? "Ekki hægt að reikna frávik" : stats.variance.toFixed(3)}</p>
+      </div>
+      <div>
+        <p><strong>Staðalfrávik:</strong></p>
+        <p>${Number.isNaN(stats.std) ? "Ekki hægt að reikna frávik" : stats.std.toFixed(3)}</p>
+      </div>
     </section>
-    <section>
+    <section class="parsed-numbers">
+      <h2>Þáttað gagnasett</h2>
       ${numbersParagraphs.join("\n")}
     </section>
   `;
@@ -31,7 +57,7 @@ export function makeHTML(stats, numberList, fileName) {
 export function makeIndex(entries) {
   let list = "";
   for (const entry of entries) {
-    const link = `<li><a href="${`${entry}.html`}">Gagnasett ${entry}</a></li>`;
+    const link = `<a href="${`${entry}.html`}"><li>Gagnasett ${entry}</li></a>`;
     list += link;
   }
 
@@ -39,13 +65,14 @@ export function makeIndex(entries) {
 }
 
 export function siteTemplate(title, content, showBack = false) {
-  const back = showBack ? '<p><a href="/">Til baka</a></p>' : "";
+  const back = showBack ? '<p class="back"><a href="/">Til baka</a></p>' : '';
   return `
   <!doctype html>
   <html>
     <head>
-      <title>${title ?? ""}</title>
+      <meta name="viewport" content="width=device-width,initial-scale=1">
       <link rel="stylesheet" href="styles.css">
+      <title>${title ?? ""}</title>
     </head>
     <body>
       <header>
